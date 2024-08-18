d14dd6a7bc8276afaf03ac89759980aee326fd90
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-18 15:21:21 +0800

    Update: Aug 18 2024

--- src/Components/List_Group.tsx
+++ src/Components/List_Group.tsx
@@ -3,19 +3,19 @@
 
 function ListGroup() {
     return (
-      <>
-        <div className="container-lg">
-          <ul className="list-group">
-            <li className="list-group-item disabled" aria-disabled="true">
-              A disabled item
-            </li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-          </ul>
-        </div>
-      </>
+        <>
+            <div className="container-lg">
+            <ul className="list-group">
+                <li className="list-group-item disabled" aria-disabled="true">
+                A disabled item
+                </li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+                <li className="list-group-item">A fourth item</li>
+                <li className="list-group-item">And a fifth one</li>
+            </ul>
+            </div>
+        </>
     );
 }
 
