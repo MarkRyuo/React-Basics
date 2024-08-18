6f4d5dda06aaf7dd7dd450b06bc4247077ba30a0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-18 15:35:55 +0800

    Update: Aug 18 2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-
+import ListGroup from "./Components/List_Group";
 
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
   return (
 
     <>
-    
+      <div><ListGroup></ListGroup></div>
     </>
 
   ) ;
