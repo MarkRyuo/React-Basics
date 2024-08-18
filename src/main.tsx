416f5dd0c95f2b63be7e91773f46847e519dcd29
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-18 17:18:30 +0800

    Update: Aug 18 2024

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css' // Import bootstrap.css
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
