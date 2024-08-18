6cb2115c23bd52fe2827746019c134f9a8fa5d4b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-18 15:45:36 +0800

    Update: Aug 18 2024

--- src/Components/List_Group.tsx
+++ src/Components/List_Group.tsx
@@ -13,7 +13,9 @@ function ListGroup() {
         // * Map method : How it works? listofNames.map(parameter => <p>{parameter}</p>)
     }
 
-
+    const getMessage() => {
+        return listofNames.map((names) => <li className="list-group-item">{names}</li>)
+    }
 
     return (
         <>
