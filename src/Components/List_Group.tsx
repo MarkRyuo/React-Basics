24fa2b8fa7766077654bee462521729da7247d42
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-18 15:21:05 +0800

    Update: Aug 18 2024

--- src/Components/List_Group.tsx
+++ src/Components/List_Group.tsx
@@ -10,9 +10,9 @@ function ListGroup() {
               A disabled item
             </li>
             <li className="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
+            <li className="list-group-item">A third item</li>
+            <li className="list-group-item">A fourth item</li>
+            <li className="list-group-item">And a fifth one</li>
           </ul>
         </div>
       </>
