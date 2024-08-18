6ed561b9efe2c55f9152ff6def2181391d2f34b2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-18 15:33:08 +0800

    Update: Aug 18 2024

--- src/Components/List_Group.tsx
+++ src/Components/List_Group.tsx
@@ -13,8 +13,6 @@ function ListGroup() {
         // * Map method : How it works? listofNames.map(parameter => <p>{parameter}</p>)
     }
 
-    listofNames.map(names => <p>{names}</p>)
-
 
 
     return (
@@ -23,11 +21,8 @@ function ListGroup() {
             <ul className="list-group">
                 <li className="list-group-item disabled" aria-disabled="true">
                     A disabled item
+                {listofNames.map((names) => <li className="list-group-item">{names}</li>)}
                 </li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
             </ul>
             </div>
         </>
