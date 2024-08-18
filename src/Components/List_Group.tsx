59b1b0fc0b884411cbb3da921d5dce49e3810257
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-18 17:14:42 +0800

    Update: Aug 18 2024

--- src/Components/List_Group.tsx
+++ src/Components/List_Group.tsx
@@ -15,7 +15,8 @@ function ListGroup() {
 
     const getMessage() => {
         return listofNames.map((names) => ( 
-            <li className="list-group-item">{names}</li>)) ;
+            <li className="list-group-item">{names}</li>
+        )) ;
     }
 
     return (
