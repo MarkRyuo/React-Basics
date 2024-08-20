74e1ee315f98de8bcf7bffc80e543a8d79b73097
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-20 10:34:33 +0800

    Update: Aug 20 2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import Conditional from "./Components/conditional";
+// import Conditional from "./Components/conditional";
 // import ListGroup from "./Components/List_Group";
+
+import HandlingEvents from "./Components/Handling_events";
 
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
   return (
 
     <>
-      <div><Conditional/></div>
+      <div><HandlingEvents/></div>
     </>
 
   ) ;
