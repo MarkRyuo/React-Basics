dcb686bc28c1a838cbb806632d37c8dfc2768955
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-20 10:32:52 +0800

    Update: Aug 20 2024

--- src/Components/Handling_events.tsx
+++ src/Components/Handling_events.tsx
@@ -18,7 +18,7 @@ function HandlingEvents () {
             <div className="container-lg">
                 <ul className="list-group">
                     <p className="h1">List of City's</p>
-                    {listofCitys.map((citys) => <li className="list-group-item">{citys}</li>)}
+                    {listofCitys.map((city) => <li className="list-group-item">{city}</li>)} {/*  */}
                 </ul>
             </div>
         
