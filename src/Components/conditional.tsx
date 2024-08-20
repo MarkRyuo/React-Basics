6e6844ed073d1118331963ed1c097fa22602266e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-20 10:13:28 +0800

    Update: Aug 20 2024

--- src/Components/conditional.tsx
+++ src/Components/conditional.tsx
@@ -7,9 +7,11 @@ function Conditional() {
     const GetAge = age >= 20 ? <p>GREATER</p> : <p>LOWER</p> ;  {/* This is a Ternary operator*/}
     
     return (
-        <> 
+
+        <>
             <h1>{GetAge}</h1>
         </>
+        
     ) ;
 }
 
