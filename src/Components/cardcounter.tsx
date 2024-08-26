4f68333ab001a4bb8384991184b7983090dec687
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 15:56:31 +0800

    Update: Aug 26 2024

--- src/Components/cardcounter.tsx
+++ src/Components/cardcounter.tsx
@@ -9,6 +9,8 @@ function Cardcounter() {
                     Counter
                     </h1>
                     <div className="card-text">
+                    <p>Count: 0</p>
+                    <button type="button">Increase</button>
                     
                     </div>
                 </div>
