7e17300b4367d3f059369ac5a663e8d88e7a3583
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 15:06:29 +0800

    Update: Aug 26 2024

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -4,7 +4,7 @@ function Card(){
     return (
         <>
             <div className="card" style={{width: "18rem;"}}>
-                <img src="..." className="card-img-top" alt="...">
+                <img src="..." className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -14,5 +14,4 @@ function Card(){
         </>
     );
 }
-
 export default Card ;
