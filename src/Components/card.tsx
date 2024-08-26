a0ff7724a1043f226381cf034db7faf06c4a0123
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 15:41:21 +0800

    Update: Aug 26 2024

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -8,7 +8,7 @@ function Card(props){
     return (
         <>
             <div className="card" style={{width: "20rem"}}>
-                <img src="..." className="card-img-top" alt="Image"/>
+                <img src="..." className="card-img-top" alt="Error image"/>
                 <div className="card-body">
                     <h5 className="card-title">{props.cardName}</h5> 
                     <p className="card-text">{props.description}</p>
