bd3b489d8ffd64ccc3e468261d383d49f7dc1536
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 15:04:24 +0800

    Update: Aug 26 2024

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -7,7 +7,7 @@ function Card(){
                 <img src="..." class="card-img-top" alt="...">
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
