34491a0274fd754c42395b389642cc782c024daf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 15:51:31 +0800

    Update: Aug 26 2024

--- src/Components/cardcounter.tsx
+++ src/Components/cardcounter.tsx
@@ -1,12 +1,12 @@
 
-function Cardcounter() {
+function Cardcounter(props) {
 
     return (
         <>
             <div className="card">
                 <div className="card-body">
                     <div className="card-title">
-                        
+                    {props.cardTitle}
                     </div>
                 </div>
             </div>
