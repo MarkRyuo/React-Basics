a06f87b7c1ca0fd7743d2ffe502db98b7e6f5030
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 15:04:03 +0800

    Update: Aug 26 2024

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -3,7 +3,7 @@
 function Card(){
     return (
         <>
-            <div class="card" style="width: 18rem;">
+            <div className="card" style="width: 18rem;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
