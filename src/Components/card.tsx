d826cc9f3fefbded67612d8cca7958223628ca3a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 15:20:28 +0800

    Update: Aug 26 2024

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -4,7 +4,7 @@
 //* Use props to make this values dynamic and changeable 
 
 
-function Card(){
+function Card(props){
     const username = "Moda"
 
 
@@ -13,7 +13,7 @@ function Card(){
             <div className="card" style={{width: "10rem;"}}>
                 <img src="..." className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">{username}</h5>
+                    <h5 className="card-title">{props.cardName}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
