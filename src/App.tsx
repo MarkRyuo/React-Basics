36c911c20ba80e0dc54a24c4531ccf32fb7c0820
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 16:44:57 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@
 function App() {
 
   return (
-
     <>
       <div className="container-lg">
-          
+          <div class="row">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+          </div>
+        </div>
       </div>
     </>
-    
-  ) ;
+  );
 }
 
 export default App ;
