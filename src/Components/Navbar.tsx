f4491ccfc58095999fd80b70bd9f09e570b6db60
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 17:24:12 +0800

    Subject: Body

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
                         <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-                        Moda
+                        Bootstrap
                     </a>
                 </div>
             </nav>
