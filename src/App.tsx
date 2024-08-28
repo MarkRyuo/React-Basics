82ccb5c83ecc6999ae9140dae8e5eee1486e5ecc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 16:35:35 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,9 @@ function App() {
   return (
 
     <>
-      <div className="container-lg">
-        <div className="row g-1">
-            <div className="col-sm-4 col-md-4">
-              <div><Card {...Users.user1}/></div>
-            </div>
-            <div className="col-sm-4 col-md-4">
-              <div><Card {...Users.user2}/></div>
-            </div>
-            <div className="col-sm-4 col-md-4">
-              <div><Card {...Users.user3}/></div>
-            </div>
-        </div>
-
-        <div>
-          <Cardcounter/>
-        </div>
-      </div>
+    
     </>
-
+    
   ) ;
 }
 
