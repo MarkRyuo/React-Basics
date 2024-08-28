fe53781d1595e154c2f86a16e4a15ba130c70bf5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 17:16:39 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,7 @@ function App() {
   return (
     <>
       <div className="container">
-          <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-              <a className="navbar-brand" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-                Moda
-              </a>
-            </div>
-          </nav>
+          
         </div>
     </>
   );
