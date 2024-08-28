17a9588c0307c9b46ca673c07def59b2c32704c6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 16:45:35 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
       <div className="container-lg">
           <div className="row">
             <div className="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
           </div>
         </div>
     </>
