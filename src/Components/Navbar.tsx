d2f1aaef83fe4d8cc527694b6f00259056ffd408
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 17:17:25 +0800

    Subject: Body

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,8 +6,7 @@ function Navbar() {
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"
-              />
+                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
               Moda
             </a>
           </div>
