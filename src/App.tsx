7ec4efd4b1354e0da96a6e79b5c664259cd9912b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 16:45:25 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <div className="container-lg">
-          <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
+          <div className="row">
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
             <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
             <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
             <div class="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
