9b61e14c956c43568f94e6fd7f62c9755325dc4e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 16:49:46 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div className="container-lg">
+      <div className="container">
           <div className="row">
             <div className="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
             <div className="col-12 col-sm-6 col-md-4 col-lg-3">Column</div>
