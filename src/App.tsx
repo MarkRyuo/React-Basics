7bd624c0c930502e2a258cd51ae89988fa8312e8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 18:23:49 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -36,16 +36,16 @@ function App() {
           <Navbar {...Text.Logo1} />
         </div>
 
-        <div className="grid">
-          <div className="g-col-md-4">
+        <div className="row grid text-center">
+          <div className="g-col-4 g-col-md-4">
             <Card {...Text.Card1}/>
           </div>
 
-          <div className="g-col-md-4">
+          <div className="g-col-6 g-col-md-4">
             <Card {...Text.Card2}/>
           </div>
 
-          <div className="g-col-md-4">
+          <div className="g-col-4 g-col-md-4">
             <Card {...Text.Card3}/>
           </div>
         </div>
