d450777fe607419219390f3cee4976e128b43b4c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 18:10:08 +0800

    Subject: Body

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -16,7 +16,7 @@ function Card(props: Cardprops) {
                 <img src="/src/assets/react.svg" className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{props.cardTitle}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">{props.cardDiscription}</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
