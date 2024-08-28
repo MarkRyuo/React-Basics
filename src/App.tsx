b7a0482137f73148534285fc8361c504816c1fb8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 17:14:12 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ function App() {
     <>
       <div className="container">
           <nav class="navbar bg-body-tertiary">
-          <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-            Bootstrap
-          </a>
-  </div>
-</nav>
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                Bootstrap
+              </a>
+            </div>
+          </nav>
         </div>
     </>
   );
