2771610fe68f3559b14b922841fb6a81755a56bf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 17:18:20 +0800

    Subject: Body

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,16 +2,16 @@
 
 function Navbar() {
     return (
-      <>
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-              Moda
-            </a>
-          </div>
-        </nav>
-      </>
+        <>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+                        Moda
+                    </a>
+                </div>
+            </nav>
+        </>
     );
 }
 
