a38414328b54dd4007913a19cbaebae018a769d9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 17:32:50 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 
 
+// * The container is the main body 
+
 function App() {
 
   return (
     <>
-      <div className="container">
+      <div className="container"> 
         <Navbar/>
       </div>
     </>
