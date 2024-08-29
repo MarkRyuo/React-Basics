456ccbb3f977fcc14d9f7f248690c522c848d2e4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-29 18:47:40 +0800

    Subject: Body

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -5,11 +5,9 @@ interface Cardsprops {
     buttonText: string ;
 }
 
-const Cards = (props : Cardsprops) => {
+export const Cards = (props : Cardsprops) => {
 
-    export const CardImages = {
-        
-    }
+    
 
     return (
         <>
@@ -17,7 +15,7 @@ const Cards = (props : Cardsprops) => {
             <div className="container-lg">
                 <div className="m-3 p-2" style={{border: "solid red"}}>
                     <div className="" style={{border: "solid red"}}>
-                        <img src="{props.Image}" alt="Image Only" style={{width: "12rem"}}/>
+                        <img src={imgsrc} alt="Image Only" style={{width: "12rem"}}/>
                         <p className="h1 mb-4">{props.CardTitle}</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus veritatis quasi id velit magni quod quam nisi quae molestias.</p>
                         <button type="button" className="btn btn-success">{props.buttonText}</button>
