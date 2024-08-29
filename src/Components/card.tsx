c68480ae60b46448ebeb17b80c0f9bdc29fb9a9f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-29 17:57:36 +0800

    Subject: Body

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -10,7 +10,7 @@ const Cards = (props : Cardsprops) => {
     return (
         <>
 
-            <div>
+            <div className="container-lg">
                 <div>
                     <p className="h1">{props.CardTitle}</p>
                     <button type="button" className="btn btn-success">{props.buttonText}</button>
