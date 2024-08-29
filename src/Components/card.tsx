2c8fe0d4394c2e52da4d148df15617f25855fa7c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-29 19:11:01 +0800

    Subject: Body

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -13,8 +13,8 @@ const Cards = (props : Cardsprops) => {
         <>
 
             <div className="container-lg">
-                <div className="m-3 p-2" >
-                    <div className="" >
+                <div className="m-3 p-2" style={{border: "solid red"}}>
+                    <div className="" style={{border: "solid red"}}>
                         <img src={props.imageSrc} alt="Image Only" style={{width: "12rem"}}/>
                         <p className="h1 mb-4">{props.CardTitle}</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus veritatis quasi id velit magni quod quam nisi quae molestias.</p>
