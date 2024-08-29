ece539922e541d5ae36786d2a7cd1a6440758c45
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-29 17:03:19 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,7 @@ function App() {
   
   return (
     <>
-      <div className="container">
-        <div className="mb-4">
-          <Navbar {...Text.Logo1} />
-        </div>
-
-        <div className="g-2 row justify-content-center">
-          <div className="mb-2 col-sm-12 col-md-4">
-            <Card {...Text.Card1}/>
-          </div>
-
-          <div className="mb-2 col-sm-12 col-md-4">
-            <Card {...Text.Card2}/>
-          </div>
-
-          <div className="mb-2 col-sm-12 col-md-4">
-            <Card {...Text.Card3}/>
-          </div>
-        </div>
-      </div>
+      
     </>
   );
 }
