7eeb97712589225efa186e30f36bd8c4bbfc699f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-30 17:32:20 +0800

    Subject: body

--- src/Componets/Cards.tsx
+++ src/Componets/Cards.tsx
@@ -25,7 +25,7 @@ const Cards = (props : Cardsprops ) => {
                     <img src={props.CardImg} alt="Error Img" />
                     <p className="h1">{props.CardTitle}</p>
                     <p className="lead">{props.CardDiscr}</p>
-                    <button className="btn btn-success"></button>
+                    <button className="btn btn-success">{props.}</button>
                 </div>
             </div>
         
