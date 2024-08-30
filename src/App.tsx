410b364cb5596615babc84fda0f815e037ff832f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-30 17:50:01 +0800

    Subject: body

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => {
     <>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-12 col-md-4 col-lg-3">
+          <div className="col-12 col-md-5 col-lg-3">
             <Cards {...Text.Card1}/>
           </div>
           <div className="col-12 col-md-4 col-lg-6">
