2ce22d8a0641e3099b514726a0d4367f1815bc1e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-30 17:30:17 +0800

    Subject: body

--- src/Componets/Cards.tsx
+++ src/Componets/Cards.tsx
@@ -27,7 +27,7 @@ const Cards = (props : Cardsprops ) => {
                     <p className="lead">{props.CardDiscr}</p>
                 </div>
                 <div className="col-12 col-md-4 col-lg-6">
-                    <img src={props.CardDiscr} alt="Error Img" />
+                    <img src={props.} alt="Error Img" />
                     <p className="h1">{props.CardTitle}</p>
                     <p className="lead">{props.CardDiscr}</p>
                 </div>
