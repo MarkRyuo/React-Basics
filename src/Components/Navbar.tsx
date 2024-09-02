a2315cf77dfc2c1589b9e467b6c40baa2e42c627
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-02 19:14:57 +0800

    Subject: Body

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <nav className="navbar bg-body-tertiary">
                 <div className="container">
-                    <a class="navbar-brand" href="#">
+                    <a className="navbar-brand" href="#">
                     <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
                     </a>
                 </div>
