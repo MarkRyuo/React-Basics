d4989b29e89102f06ac3bf8c0c890432a236a50d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-04 17:20:56 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@
 
 // * The container is the main body
 
+import TodoCards from "./Components/TodoCards";
+import TodoInput from "./Components/TodoInput";
+
 const App = () => {
 
 
     return (
-        <>
-            
-        </>
+        <main>
+
+            <TodoCards/>
+            <TodoInput/>
+
+        </main>
     );
 };
 
