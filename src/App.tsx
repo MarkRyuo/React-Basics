afb6409eddadde3433ad0acb50953a70dfc7378c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-04 17:07:17 +0800

    Subject: Body

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,11 @@
 // * The container is the main body
 
 const App = () => {
-    const Text = {
-        Card1: {
-            CardImg: "/src/assets/Card1.jpg",
-            CardTitle: "Basic",
-            CardDiscr: "$100",
-            CardBtn: "Buy",
-    },
 
-        Card2: {
-            CardImg: "/src/assets/Card2.jpg",
-            CardTitle: "Premium",
-            CardDiscr: "$700",
-            CardBtn: "Credit",
-    },
-
-        Card3: {
-            CardImg: "/src/assets/Card3.jpg",
-            CardTitle: "Standard",
-            CardDiscr: "$400",
-            CardBtn: "Credit",
-    },
-};
 
     return (
         <>
-            <div className="container-lg mt-4">
-                <div><Navbar/></div>
-                <div className="row text-center justify-content-center align-items-center">
-                <div className="col-12 col-md-6 col-lg-3">
-                    <Cards {...Text.Card1} />
-                </div>
-                <div className="col-12 col-md-3 col-lg-5">
-                    <Cards {...Text.Card2} />
-                </div>
-                <div className="col-12 col-md-3 col-lg-4">
-                    <Cards {...Text.Card3} />
-                </div>
-                </div>
-            </div>
+            
         </>
     );
 };
