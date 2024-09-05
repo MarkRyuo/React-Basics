659afdb8a35157bb6b9f3973d7a54649dc6dff14
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-05 19:02:30 +0800

    Subject: Body

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -13,10 +13,10 @@ export default function TodoList() {
             {todos.map((todoo, todoIndex) => {
 
                 return (
-                  <li key={todoIndex}>
-                    {todoo}
-                    <i className="fa-solid fa-pen-to-square"></i>
-                  </li>
+                    <li key={todoIndex}>
+                        {todoo}
+                        <i className="fa-solid fa-pen-to-square"></i>
+                    </li>
                 );
             })}
     
