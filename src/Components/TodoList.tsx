4ab5deb5aee197849113939b3ee7592227f340b8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-06 10:13:44 +0800

    Subject: Body

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -20,7 +20,7 @@ export default function TodoList() {
                                 {todoo}
                             </div>
 
-                            <div>
+                            <div className="">
                                 <button type="button" onClick={() => {}}><i className="fa-solid fa-pen-to-square"></i></button>
                             </div>
                         </li>
