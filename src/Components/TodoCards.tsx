9c9841c964730c84fe3a4e630169990fd7a7f942
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-07 15:46:03 +0800

    Subject: Body

--- src/Components/TodoCards.tsx
+++ src/Components/TodoCards.tsx
@@ -7,7 +7,6 @@ export default function TodoCards() {
             <li className="todoItem">
                 <div className="actionContainer">
                     <div className="btn-icons">
-                        <button type="button" onClick={() => {}}>
                             <i className="fa-solid fa-pen-to-square"></i>
                         <button type="button" onClick={() => {}}>
                             <i className="fa-solid fa-trash"></i>
