855fcbaf691e1f40609204b42090b5580830eac6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-07 15:56:14 +0800

    Subject: Body

--- src/Components/TodoInput.tsx
+++ src/Components/TodoInput.tsx
@@ -5,11 +5,7 @@ export default function TodoInput() {
   return (
     <header>
           <input className="input-text" type="text" placeholder="Enter Todo......"/>
-          <button type="button" onClick={() => {
-            
-            return 
-
-          }}>Add</button>
+          <button type="button" onClick={() => {}}>Add</button>
     </header>
   )
 }
